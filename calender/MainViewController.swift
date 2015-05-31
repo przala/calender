724c32b853430060b3995ee6992d9a6334ecf0ba
Pushparaj Zala <przala92@gmail.com>
2015-06-01 00:46:49 +0530

    added home view controller

---
+++ calender/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  calender
+//
+//  Created by Pushparaj Zala on 6/1/15.
+//  Copyright (c) 2015 Pushparaj Zala. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
